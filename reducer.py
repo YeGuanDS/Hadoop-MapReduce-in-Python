33c047f5b56b08211e5294e2db255d3a7067a75a
YeGuanDS <34905933+YeGuanDS@users.noreply.github.com>
2018-04-19 17:20:00 -0400

    upload reducer.py

---
+++ reducer.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Hadoop reducer in Python
+"""
+from itertools import groupby
+from operator import itemgetter
+import sys
+
+def read_mapper_output(file, separator='\t'):
+    for line in file:
+        yield line.rstrip().split(separator, 1)
+
+def main(separator='\t'):
+    # input comes from mapper.py
+    data = read_mapper_output(sys.stdin, separator=separator)
+    # groupby groups multiple word-count pairs by word,
+    # and creates an iterator that returns consecutive keys and their group:
+    #   current_word - string containing a word (the key)
+    #   group - iterator yielding all current_word,count pairs
+    for current_word, group in groupby(data, itemgetter(0)):
+        try:
+            total_count = sum(int(count) for current_word, count in group)
+            print "%s%s%d" % (current_word, separator, total_count)
+        except ValueError:
+            # if count is not a number, discard this item
+            pass
+
+if __name__ == "__main__":
+    main()
